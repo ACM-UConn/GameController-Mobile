f61884451ee9e7873cae15a98982f5056fff572b
TrevAnd1 <trevor.p.anderson@uconn.edu>
2020-10-28 22:29:21 -0400

    update home screen

--- components/HomeScreen/HomeScreen.js
+++ components/HomeScreen/HomeScreen.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
+import ControllerList from './ControllerList';
+import HomeActionBar from './HomeActionBar';
 
-export default function HomeScreen() {
-  return (
-      null
-  );
+export default function HomeScreen(props) {
+
+  // when abraham (homeactionbar) sends me the screen they press
+  const screenCallback = (screen) => {
+    props.changeScreen(screen)
+  }
+
+  
+  if (props.shouldRender) {
+    return(
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.header}> GameController </Text>
+      </View>
+      <ControllerList></ControllerList>
+      <HomeActionBar screenRequest={screen => screenCallback(screen)}></HomeActionBar>
+    </View>)
+  } else {
+    return(null)
+  }
 }
 
 const styles = StyleSheet.create({
-    
+    container: {
+      flex: 1,
+    },
+
+    header: {
+      color: 'blue',
+      textAlign: 'center',
+      marginTop: 30,
+      fontSize: 50,
+    }
 });
