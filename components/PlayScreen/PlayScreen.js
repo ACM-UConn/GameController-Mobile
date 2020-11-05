6d35f936ad9a100e6875770e0c79fc8a367c5463
TrevAnd1 <trevor.p.anderson@uconn.edu>
2020-11-04 21:07:34 -0500

    added code for PlayScreen

--- components/PlayScreen/PlayScreen.js
+++ components/PlayScreen/PlayScreen.js
@@ -1,12 +1,42 @@
-import React from 'react';
+import  React, { useState } from 'react';
 import { StyleSheet } from 'react-native';
+import ControllerPlay from './ControllerPlay.js'
+import ControllerConnect from './ControllerConnect.js'
+import { useState } from 'react';
 
-export default function PlayScreen() {
+export default function PlayScreen(props) {
+  
+  const [connected, setConnected] = useState(False); 
+
+  const connectRender = (status) => {
+    setConnected(status);
+  }
+  
+  if(props.shouldRender) {
+    if(connected){
+      return(
+        <View style={styles.container}>
+          <ControllerPlay></ControllerPlay>
+        </View>
+      )
+    } else {
+        return(
+          <View style={styles.container}>
+            <ControllerConnect done={(status) => connectRender(status)}></ControllerConnect>
+          </View>
+        )
+    }
+  }
   return (
       null
   );
 }
 
+// get info from App.js to tell controller list to render
+
 const styles = StyleSheet.create({
-    
+  
+  container: {
+    flex: 1,
+  },
 });
