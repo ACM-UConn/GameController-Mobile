87754f1b6fe97717d40bf467e919e431a8a6a230
brandonmino <brandon.mino@uconn.edu>
2020-11-04 22:23:57 -0500

    fix Neil's life (rip)

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './components/HomeScreen/HomeScreen';
 import PlayScreen from './components/PlayScreen/PlayScreen';
 import EditScreen from './components/EditScreen/EditScreen';
+import * as Permissions from 'expo-permissions';
 
 export default function App() {
   const [currentScreen, setCurrentScreen] = useState("home");
+  const [cameraPermission, setCameraPermission] = useState(false);
 
   const changeScreen = (newScreen) => {
     if (newScreen === "home" || newScreen === "play" || newScreen === "edit") {
@@ -13,10 +15,19 @@ export default function App() {
     }
   }
 
+  useEffect(() => {
+    requestCameraPermission();
+  }, [])
+
+  requestCameraPermission = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    setCameraPermission(status === 'granted');
+  };
+
   return (
     <View style={styles.container}>
       <HomeScreen shouldRender={currentScreen === "home"} changeScreen={(screen) => {changeScreen(screen)}}></HomeScreen>
-      <PlayScreen shouldRender={currentScreen === "play"} changeScreen={(screen) => {changeScreen(screen)}}></PlayScreen>
+      <PlayScreen cameraPermission={cameraPermission} shouldRender={currentScreen === "play"} changeScreen={(screen) => {changeScreen(screen)}}></PlayScreen>
       <EditScreen shouldRender={currentScreen === "edit"} changeScreen={(screen) => {changeScreen(screen)}}></EditScreen>
     </View>
   );
