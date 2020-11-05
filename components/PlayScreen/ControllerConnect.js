f556d97f605943c14cd520ee55937c76b8dada18
jshui17 <jonathanshui56@gmail.com>
2020-11-04 22:02:13 -0500

    Updated controller connect

--- components/PlayScreen/ControllerConnect.js
+++ components/PlayScreen/ControllerConnect.js
@@ -1,21 +1,45 @@
 import React from 'react';
-import { ImagePropTypes, StyleSheet } from 'react-native';
+import { ImagePropTypes, StyleSheet, View } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
-export default function ControllerConnect() {
-  const [hasPermission, setHasPermission] = useState(null);
+export default function ControllerConnect(props) {
+  //cameraPerms
   const [scanned, setScanned] = useState(false);
-  const screenCallback = (screen)=>{
-    props.changeScreen(screen)
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    props.done(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+  
+  if(props.cameraPerms==null){
+    //waiting for permission
+    return(
+      <View>
+        <Text>Waiting for permission</Text>
+      </View>
+    )
+  }
+  if(props.cameraPerms==false){
+    //denied permission
+    return(
+      <View>
+        <Text>Permission Denied</Text>
+      </View>
+    )
+  }
+  if(props.cameraPerms==true){
+    //permissions granted
+   // <View>Permission Granted</View>
+    return(
+      <View>
+        <BarCodeScanner onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}></BarCodeScanner>
+      </View>
+    )
   }
 
-  
-  if(props.shouldRender==True){
-  return (
-      null
-  );
   }
-}
+
 
 const styles = StyleSheet.create({
     
