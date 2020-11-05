8cf135648015072861f229c93dfd8aa763adac30
M4nik <maniksoomro@gmail.com>
2020-11-04 22:03:26 -0500

    Controller List added

--- components/HomeScreen/ControllerList.js
+++ components/HomeScreen/ControllerList.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, FlatList, View, Text } from 'react-native';
+
+const Data = [
+    {
+        id: 'fksfii4h3546mnmfjfj3',
+        title: 'Xbox Controller'
+    }
+]
 
 export default function ControllerList() {
+
+    const renderController = itemData => (
+        <View style={styles.controllerItem}>
+            <Text>{itemData.item.title}</Text>
+        </View>
+    )
+
+
   return (
-      null
+        <FlatList 
+            data={Data}
+            renderItem={renderController}
+            keyExtractor={item => item.id}
+        />
   );
 }
 
 const styles = StyleSheet.create({
-    
+    itemText: {
+        fontSize: 40
+    },
+    controllerItem: {
+        width: '40%',
+        height: '25%',
+        marginTop: 70,
+        marginRight: 200,
+        alignContent: 'center',
+        backgroundColor: '#f5f5f5',
+        overflow: 'hidden',
+    },
+
 });
