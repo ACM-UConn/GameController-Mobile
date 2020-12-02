df83c88019f23518e5bcaa4353f5da5b790ec645
TrevAnd1 <trevor.p.anderson@uconn.edu>
2020-12-01 21:16:27 -0500

    added code for the edit screen

--- components/EditScreen/EditScreen.js
+++ components/EditScreen/EditScreen.js
@@ -1,12 +1,64 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { ImagePropTypes, StyleSheet, View, Text } from 'react-native';
+import EditActionBar from './EditActionBar.js';
+import SideMenu from './SideMenu.js';
 
-export default function EditScreen() {
-  return (
+export default function EditScreen(props) {
+
+  const [visibility, setVisibility] = useState(false);
+
+  const visible = () => {
+    setVisibility(!visibility);
+  }
+
+  const handleAddButton = () => {
+    console.log("Look i made a button :) ")
+  }
+
+  if(props.shouldRender){
+    return(
+      <View style={styles.container}>
+        <View style={styles.body}>
+          <Text>This is the body.</Text>
+        </View>
+        <View style={styles.menu}>
+          <SideMenu visible={visibility}></SideMenu>
+        </View>
+        <View style={styles.actionBar}>
+          <EditActionBar visible={() => visible()} addButton={() => handleAddButton()}></EditActionBar>
+        </View>
+      </View>
+    );
+  }
+  else {
+    return(
       null
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    
+    container: {
+      flex: 1,
+    },
+
+    body: {
+      flex: 0.8,
+      marginTop: 50
+    },
+
+    actionBar: {
+      flex: 0.2,
+      marginLeft: 15,
+      marginRight: 15,
+      marginBottom: 50,
+    },
+
+    menu: {
+      position: 'absolute',
+      width: 100,
+      height: 300,
+      top: 0,
+      right: 0,
+    }
 });
